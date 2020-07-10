e26b13576dba627060d1bf17721abed88094e458
akhil <akkeakhilkumar20@gmail.com>
2020-07-10 14:58:07 +0530

    Updates

---
+++ precache-manifest.cbbc3716562769a8c0273037f5d6f729.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3581f5e9aa55ac16a3c91eb517bcbca",
+    "url": "/Monster-project/index.html"
+  },
+  {
+    "revision": "1eea7b3ec94ec993add0",
+    "url": "/Monster-project/static/css/main.64f7e18f.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/Monster-project/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Monster-project/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1eea7b3ec94ec993add0",
+    "url": "/Monster-project/static/js/main.1b9ccdac.chunk.js"
+  },
+  {
+    "revision": "6f0889761a2519c35177",
+    "url": "/Monster-project/static/js/runtime-main.ac694b71.js"
+  }
+]);
